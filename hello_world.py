1b897b9df93377694b786547bff85382c475e64d
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2016-04-06 23:49:49 -0300

    Implementado algoritmo Smith-Waterman

--- hello_world.py
+++
@@ -1,5 +0,0 @@
-try:
-    numero = int(input("Digite um numero par:"))
-    print(numero)
-except ValueError:
-    print("Digite apenas numeros inteiros")
---
+++ programacao_para_bioinformatica/.#ex5.py
@@ -0,0 +1,1 @@
+ricardo@gentoo_desktop.27877:1458746721
