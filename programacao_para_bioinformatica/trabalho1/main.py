2b79f87152c83348b0bbf524958a4458f353e090
Ricardo Medeiros <ricardo.medeiros.costa@gmail.com>
2016-04-18 02:05:29 -0300

    Corrigido leitura antisense

--- programacao_para_bioinformatica/trabalho1/main.py
+++ programacao_para_bioinformatica/trabalho1/main.py
@@ -1,15 +1,16 @@
 import re
 
-def imprimir(sequencia1, sequencia2, leitura):
+def imprimir(sequencia1, sequencia2, leitura, antisense=False):
     try:
         sequencia = sequencia1[(leitura-1):]
+        leitura *= -1 if antisense else 1
         
         print("Leitura", leitura, sequencia,"Quantidade encontrado:", sequencia.count(sequencia2), "\nPosições: ", [(s+1) for s in [x.start() for x in re.finditer(sequencia2, sequencia)]])
     except:
         print("Leitura", leitura, sequencia,"Quantidade encontrado:", sequencia.count(sequencia2), "\nPosições: Não há ocorrência")
         
 def obter_antisense(sequencia):
-    return sequencia.replace("A", "X").replace("T","A").replace("X","T").replace("G","X").replace("C","G").replace("X","C")
+    return sequencia.replace("A", "X").replace("T","A").replace("X","T").replace("G","X").replace("C","G").replace("X","C")[::-1]
 
 def main():
     try:         
@@ -29,7 +30,7 @@ def main():
         for i in range(1,4):
             imprimir(sequencia, sequencia2, i)
         for i in range(1,4):
-            imprimir(antisense, sequencia2, i)
+            imprimir(antisense, sequencia2, i, antisense=True)
 
     except ValueError:
          print("Você digitou uma sequência inválida. As sequências devem ter apenas caracteres ATGC sendo que a segunda deve ser apenas ATGC e ter apenas 3 bases.\n")
